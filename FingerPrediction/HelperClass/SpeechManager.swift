68984c0312ec42a6048f05e3bbb30ced7febc545
talaakash <akasht.noble@gmail.com>
2024-04-09 16:27:20 +0530

    Convert Text into speech

--- FingerPrediction/HelperClass/SpeechManager.swift
+++ FingerPrediction/HelperClass/SpeechManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import AVFAudio
+
+class SpeechManager{
+    static let shared = SpeechManager()
+    private let synthesizer = AVSpeechSynthesizer()
+    private init(){ }
+    
+    func speak(text: String){
+        stop()
+        let speechUtterance = AVSpeechUtterance(string: text)
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        speechUtterance.rate = 0.5
+        speechUtterance.pitchMultiplier = 1.0
+        synthesizer.speak(speechUtterance)
+    }
+    
+    private func stop(){
+        synthesizer.stopSpeaking(at: .immediate)
+    }
+}
