6b93537667e02a30bd13886bbbb764ec9f561e7a
talaakash <akasht.noble@gmail.com>
2024-04-09 16:26:54 +0530

    Sound Manager For play Process Sound

--- FingerPrediction/HelperClass/SoundManager.swift
+++ FingerPrediction/HelperClass/SoundManager.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import AVFAudio
+
+class SoundManager {
+    static let shared = SoundManager()
+    var engine = AVAudioEngine()
+    var playerNode = AVAudioPlayerNode()
+    
+    private init() {
+        engine.attach(playerNode)
+        engine.connect(playerNode, to: engine.mainMixerNode, format: nil)
+        do {
+            try engine.start()
+        } catch {
+            print("Error starting audio engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func playWeirdSound() {
+        let audioFileURL = Bundle.main.url(forResource: "Process", withExtension: "mp3")!
+        let audioFile = try! AVAudioFile(forReading: audioFileURL)
+        let audioFormat = audioFile.processingFormat
+        let audioFrameCount = UInt32(audioFile.length)
+        let audioBuffer = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: audioFrameCount)
+        try! audioFile.read(into: audioBuffer!)
+        playerNode.scheduleBuffer(audioBuffer!, completionHandler: nil)
+        playerNode.play()
+    }
+    
+    func stopSound() {
+        playerNode.stop()
+    }
+}
