4a2061fb4943bf505fc1d672df6c58e512695768
talaakash <akasht.noble@gmail.com>
2024-04-09 16:26:25 +0530

    Label With Blinking Cursor

--- FingerPrediction/CustomClasses/BlinkingCursorUILabel.swift
+++ FingerPrediction/CustomClasses/BlinkingCursorUILabel.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UIKit
+
+class BlinkingCursorLabel: UILabel {
+    private var cursorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private var isCursorVisible = false
+    private var cursorTimer: Timer?
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupCursor()
+        startBlinking()
+    }
+    
+    private func setupCursor() {
+        cursorView.removeFromSuperview()
+        textColor = UIColor.lightGray
+        font = UIFont(name: "Faelorehn", size: 40)
+        addSubview(cursorView)
+        cursorView.frame = CGRect(x: textWidth(), y: 0, width: 5, height: frame.height)
+    }
+    
+    private func textWidth() -> CGFloat {
+        guard let text = text else { return 0 }
+        let attributes: [NSAttributedString.Key: Any] = [.font: font as Any]
+        let textSize = (text as NSString).size(withAttributes: attributes)
+        return textSize.width
+    }
+    
+    private func startBlinking() {
+        cursorTimer?.invalidate()
+        cursorTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [weak self] _ in
+            self?.isCursorVisible.toggle()
+            self?.cursorView.isHidden = !self!.isCursorVisible
+        }
+    }
+}
