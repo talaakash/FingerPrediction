27cfb5c88d2889bd118aca85bb6dc74332ae0136
talaakash <akasht.noble@gmail.com>
2024-04-10 10:42:22 +0530

    Added smoky animation

--- FingerPrediction/Extentions/UIImageView+Extention.swift
+++ FingerPrediction/Extentions/UIImageView+Extention.swift
@@ -27,4 +27,35 @@ extension UIImageView{
             self.kf.setImage(with: imageResource)
         }
     }
+    
+    func addSmokyAnimation() {
+        let emitterLayer = CAEmitterLayer()
+        emitterLayer.emitterPosition = CGPoint(x: bounds.midX, y: bounds.midY)
+        emitterLayer.emitterSize = bounds.size
+        emitterLayer.emitterShape = .circle
+        
+        let cell = CAEmitterCell()
+        cell.birthRate = 100
+        cell.lifetime = 5.0
+        cell.velocity = 50
+        cell.velocityRange = 50
+        cell.emissionLongitude = .pi
+        cell.emissionRange = .pi / 4
+        cell.scale = 0.1
+        cell.scaleRange = 0.3
+        cell.alphaSpeed = -0.025
+        
+        // Define the color for the smoke effect
+        cell.color = UIColor(white: 1.0, alpha: 0.3).cgColor
+        cell.contents = UIImage(named: "Smoke")?.cgImage // Use any small image for particle, or set to nil
+        
+        emitterLayer.emitterCells = [cell]
+        
+        layer.addSublayer(emitterLayer)
+    }
+    
+    func removeSmokyAnimation(){
+        self.layer.sublayers?.removeAll()
+    }
+    
 }
