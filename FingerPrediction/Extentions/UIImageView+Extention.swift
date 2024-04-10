5cebf7e1027dcf9fc92f3c9d8c00e89b03632c83
talaakash <akasht.noble@gmail.com>
2024-04-10 11:47:38 +0530

    added scaling in Image

--- FingerPrediction/Extentions/UIImageView+Extention.swift
+++ FingerPrediction/Extentions/UIImageView+Extention.swift
@@ -59,3 +59,13 @@ extension UIImageView{
     }
     
 }
+
+extension UIImage{
+    func scaledImage(withScale scale: CGFloat) -> UIImage? {
+            let newSize = CGSize(width: size.width * scale, height: size.height * scale)
+            UIGraphicsBeginImageContextWithOptions(newSize, false, scale)
+            defer { UIGraphicsEndImageContext() }
+            draw(in: CGRect(origin: .zero, size: newSize))
+            return UIGraphicsGetImageFromCurrentImageContext()
+        }
+}
